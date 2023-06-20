62ff708304a2ee23c49dd023f3b343a044625874
abdecd <null>
2023-06-20 15:27:54 +0800

    t

--- src/Components/BackstagePage/SideBar.tsx
+++ src/Components/BackstagePage/SideBar.tsx
@@ -58,7 +58,7 @@ function SideBarFolder({level,text,children}: {
             text={
                 <>
                 <div>{text}</div>
-                <div className={"sidebar-folder-arrow"+(expand?" expand":"")}>
+                <div className="sidebar-folder-arrow">
                     <svg style={{width: "1em", height: "1em", verticalAlign: "middle", fill: "currentColor", overflow: "hidden"}} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1452"><path d="M535.12 711.6L870.528 355.2a29.376 29.376 0 0 0 0-42.352 31.376 31.376 0 0 0-43.52 0l-315.2 334.912-315.2-334.912a31.376 31.376 0 0 0-43.52 0 29.376 29.376 0 0 0 0 42.352l335.408 356.4a36.272 36.272 0 0 0 46.624 0z" p-id="1453"></path></svg>
                 </div>
                 </>
