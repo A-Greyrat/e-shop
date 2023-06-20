c68bfc0b2ec279ac1ba4e43bbced5f3ca76d57d6
abdecd <null>
2023-06-20 12:48:03 +0800

    t

--- src/Pages/DetailPage.tsx
+++ src/Pages/DetailPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Covers from '../Components/DetailPage/Covers';
 import PurchaseBlock from '../Components/DetailPage/PurchaseBlock';
 import BusinessIntro from '../Components/DetailPage/BusinessIntro';
@@ -12,6 +12,7 @@ import './DetailPage.css';
 export default function DetailPage() {
     var { goodsId: goodsIdStr } = useParams();
     var goodsId = parseInt(goodsIdStr || "1");
+    const nav = useNavigate();
 
     const [goodsDetail, setGoodsDetail] = useState<{
         name: string,
@@ -43,12 +44,18 @@ export default function DetailPage() {
 
     return (
         <div className='detail-page-root'>
-            <BusinessIntro
-                avatar={businessIntro?.avatar}
-                name={businessIntro?.name}
-                goodsRank={businessIntro?.goodsRank}
-                businessRank={businessIntro?.businessRank}
-            />
+            <div>
+                <div onClick={() => {nav("/")}}>
+                    <svg style={{width: "1em", height: "1em", verticalAlign: "middle", fill: "currentColor", overflow: "hidden"}} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1484"><path d="M665.6 938.666667c-12.8 0-21.333333-4.266667-29.866667-12.8l-341.333333-332.8c-21.333333-21.333333-34.133333-51.2-34.133333-81.066667s12.8-59.733333 34.133333-81.066667l341.333333-332.8c17.066667-17.066667 42.666667-17.066667 59.733334 0 17.066667 17.066667 17.066667 42.666667 0 59.733334l-341.333334 332.8c-4.266667 4.266667-8.533333 12.8-8.533333 21.333333s4.266667 12.8 8.533333 21.333333l341.333334 332.8c17.066667 17.066667 17.066667 42.666667 0 59.733334-4.266667 8.533333-17.066667 12.8-29.866667 12.8z" fill="#221E1F" p-id="1485"></path></svg>
+                </div>
+                <BusinessIntro
+                    avatar={businessIntro?.avatar}
+                    name={businessIntro?.name}
+                    goodsRank={businessIntro?.goodsRank}
+                    businessRank={businessIntro?.businessRank}
+                />
+            </div>
+            
             <div className='detail-page-product'>
                 <Covers imageArr={[ajax.getCoverImgSrc(goodsId)]} />
                 <PurchaseBlock
