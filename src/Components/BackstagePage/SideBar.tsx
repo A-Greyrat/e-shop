70a366b96071bb63f8a96d6de8d1a3e05aa54f42
abdecd <null>
2023-06-20 15:36:55 +0800

    t

--- src/Components/BackstagePage/SideBar.tsx
+++ src/Components/BackstagePage/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 import './SideBar.css'
@@ -17,7 +17,7 @@ export default function SideBar({objArr}: {
     const mapComps = (level: number,objArr?: SideBarJSCell[]) => {
         return objArr?.map(n => {
             if (n.type=='item') {
-                return <SideBarItem level={level} text={n.text} clickFn={()=>nav(n.url)}/>;
+                return <SideBarItem level={level} content={n.text} clickFn={()=>nav(n.url)}/>;
             } else if (n.type=='folder') {
                 return <SideBarFolder level={level} text={n.text}>{
                     mapComps(level+1,n.children)
@@ -35,13 +35,13 @@ export default function SideBar({objArr}: {
     )
 }
 
-function SideBarItem({level,text,clickFn}: {
+function SideBarItem({level,content,clickFn}: {
     level: number;
-    text: any;
+    content: any;
     clickFn: Function;
 }) {
     return <div className='sidebar-item' style={{paddingLeft: 10*level+"px"}} onClick={ev=>clickFn(ev)}>
-        {text}
+        {content}
     </div>
 }
 
@@ -55,13 +55,13 @@ function SideBarFolder({level,text,children}: {
     return <div className={'sidebar-folder'+(expand?" expand":"")}>
         <SideBarItem
             level={level}
-            text={
-                <>
-                <div>{text}</div>
-                <div className="sidebar-folder-arrow">
-                    <svg style={{width: "1em", height: "1em", verticalAlign: "middle", fill: "currentColor", overflow: "hidden"}} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1452"><path d="M535.12 711.6L870.528 355.2a29.376 29.376 0 0 0 0-42.352 31.376 31.376 0 0 0-43.52 0l-315.2 334.912-315.2-334.912a31.376 31.376 0 0 0-43.52 0 29.376 29.376 0 0 0 0 42.352l335.408 356.4a36.272 36.272 0 0 0 46.624 0z" p-id="1453"></path></svg>
+            content={
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                    <div>{text}</div>
+                    <div className="sidebar-folder-arrow">
+                        <svg style={{width: "1em", height: "1em", verticalAlign: "middle", fill: "currentColor", overflow: "hidden"}} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1452"><path d="M535.12 711.6L870.528 355.2a29.376 29.376 0 0 0 0-42.352 31.376 31.376 0 0 0-43.52 0l-315.2 334.912-315.2-334.912a31.376 31.376 0 0 0-43.52 0 29.376 29.376 0 0 0 0 42.352l335.408 356.4a36.272 36.272 0 0 0 46.624 0z" p-id="1453"></path></svg>
+                    </div>
                 </div>
-                </>
             }
             clickFn={()=>setExpand(a=>!a)}/>
         <div className='sidebar-folder-children'>{children}</div>
